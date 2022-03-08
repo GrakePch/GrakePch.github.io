ad7d2292b5d3021004e6447f0f128e93cac8cfa6
GrakePCH <1152932438@qq.com>
2022-03-08 05:03:41 -0800

    Minor update

--- src/pages/modules/floatSettings.js
+++ src/pages/modules/floatSettings.js
@@ -9,91 +9,91 @@ import { mdiBrightness4, mdiBrightness7, mdiCogOutline, mdiTranslate } from '@md
 import { globalVars } from "./globalVars";
 
 export default function FloatSettings(props) {
-    const theme = useTheme();
-    const [isSettingsOpen, setIsSettingOpen] = useState(false);
+  const theme = useTheme();
+  const [isSettingsOpen, setIsSettingOpen] = useState(false);
 
-    useEffect(() => {
+  useEffect(() => {
 
 
-        const onScroll = e => {
-            // if (noShadowAtTop) {
-            //   if (window.pageYOffset <= 0) {
-            //     setNoShadow(true);
-            //   } else {
-            //     setNoShadow(false);
-            //   }
-            // }
-            // console.log(window.pageYOffset);
-            setIsSettingOpen(false);
-        };
-        window.addEventListener("scroll", onScroll);
+    const onScroll = e => {
+      // if (noShadowAtTop) {
+      //   if (window.pageYOffset <= 0) {
+      //     setNoShadow(true);
+      //   } else {
+      //     setNoShadow(false);
+      //   }
+      // }
+      // console.log(window.pageYOffset);
+      setIsSettingOpen(false);
+    };
+    window.addEventListener("scroll", onScroll);
 
-        return () => window.removeEventListener("scroll", onScroll);
-    })
-    
-    // Read theme from localStorage
-    if (localStorage.getItem('isThemeLight') === 'false') {
-        props.setTheme(false);
-    }
-    // Read language from localStorage
-    if (localStorage.getItem('lang') === "1") {
-        props.setLang(1);
-    }
+    return () => window.removeEventListener("scroll", onScroll);
+  })
 
-    return (
-        <div style={{
-            position: "fixed",
-            right: 8,
-            top: "50%",
-            transform: "translateY(-50%)",
-            height: isSettingsOpen ? 48 * 3 : 48,
-            overflow: "hidden",
-            transition: "height .2s cubic-bezier(.5,0,0,1)",
-            zIndex: 99,
-        }}>
-            <CardActionArea
-            style={{
-                width: 48,
-                height: 48,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-                onClick={() => setIsSettingOpen(!isSettingsOpen)}
-            >
-                <Icon size={1} path={mdiCogOutline}
-                    style={{
-                        transform: isSettingsOpen ? "rotate(210deg)" : "rotate(0)",
-                        transition: "transform .2s cubic-bezier(.5,0,0,1)",
-                    }}
-                />
-            </CardActionArea>
-            <div style={{ backgroundColor: theme.palette.background.invert, color: theme.palette.background.default }}>
-                <CardActionArea
-                style={{
-                    width: 48,
-                    height: 48,
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-                    onClick={() => props.themeToggle()}
-                >
-                    <Icon size={1} path={globalVars.isThemeLight? mdiBrightness4: mdiBrightness7} />
-                </CardActionArea>
-                <CardActionArea
-                style={{
-                    width: 48,
-                    height: 48,
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-                    onClick={() => props.langToggle()}
-                >
-                    <Icon size={1} path={mdiTranslate} />
-                </CardActionArea>
-            </div>
-        </div>
-    )
+  // Read theme from localStorage
+  if (localStorage.getItem('isThemeLight') === 'false') {
+    props.setTheme(false);
+  }
+  // Read language from localStorage
+  if (localStorage.getItem('lang') === "1") {
+    props.setLang(1);
+  }
+
+  return (
+    <div style={{
+      position: "fixed",
+      right: 8,
+      top: "50%",
+      transform: "translateY(-50%)",
+      height: isSettingsOpen ? 48 * 3 : 48,
+      overflow: "hidden",
+      transition: "height .2s cubic-bezier(.5,0,0,1)",
+      zIndex: 99,
+    }}>
+      <CardActionArea
+        style={{
+          width: 48,
+          height: 48,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        onClick={() => setIsSettingOpen(!isSettingsOpen)}
+      >
+        <Icon size={1} path={mdiCogOutline}
+          style={{
+            transform: isSettingsOpen ? "rotate(210deg)" : "rotate(0)",
+            transition: "transform .2s cubic-bezier(.5,0,0,1)",
+          }}
+        />
+      </CardActionArea>
+      <div style={{ backgroundColor: theme.palette.background.invert, color: theme.palette.background.default }}>
+        <CardActionArea
+          style={{
+            width: 48,
+            height: 48,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onClick={() => props.themeToggle()}
+        >
+          <Icon size={1} path={globalVars.isThemeLight ? mdiBrightness4 : mdiBrightness7} />
+        </CardActionArea>
+        <CardActionArea
+          style={{
+            width: 48,
+            height: 48,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onClick={() => props.langToggle()}
+        >
+          <Icon size={1} path={mdiTranslate} />
+        </CardActionArea>
+      </div>
+    </div>
+  )
 }
